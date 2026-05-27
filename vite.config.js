e3220e1e492f4c806b96b523046db3e8c98b114e
vinitha <sagarvinitha2@gmail.com>
2026-05-27 10:39:48 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
+  base: '/PopX/',
+
   plugins: [
     react(),
     babel({
