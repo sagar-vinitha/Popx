092e72b67eda39bc3fa104cb3000e7723ffbc756
vinitha <sagarvinitha2@gmail.com>
2026-05-27 10:37:18 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()]
+    }),
+  ],
+});
